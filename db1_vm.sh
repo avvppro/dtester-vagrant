5b68508ed182f64449306df31ec2f0d8bfb40626
avvppro <avvppro@gmail.com>
2020-08-10 00:47:01 +0300

    change ip

--- db1_vm.sh
+++ db1_vm.sh
@@ -13,6 +13,7 @@ database_config() {
     #!/usr/bin/expect -f
     set timeout 3
     spawn mysql -u root -p
+    sleep 2
     expect "Enter password:"
     send "[lindex \$argv 0]
     "
@@ -30,7 +31,7 @@ database_config() {
     "
     sleep 1
     expect "mysql>"
-    send "GRANT ALL PRIVILEGES ON dtapi2.* TO 'username'@'%';
+    send "GRANT ALL PRIVILEGES ON dtapi2.* TO 'username'@'%'  WITH GRANT OPTION;
     "
     sleep 1
     expect "mysql>"
@@ -44,17 +45,16 @@ _EOF
     sudo chmod 777 ./my.expect
     sudo grep 'temporary password' /var/log/mysqld.log | sed 's|.*: ||' >./1.txt
     tmp_pass=$(cat 1.txt)
-    sudo echo "$tmp_pass"
-    sudo ./my.expect $tmp_pass
+    expect ./my.expect $tmp_pass
     rm ./my.expect
 }
 load_dump() {
     wget https://dtapi.if.ua/~yurkovskiy/dtapi_full.sql
     mysql -u root --password=RootPWD1@ dtapi2 < ./dtapi_full.sql
-    sudo systemctl restart mysqld
     sudo chmod 666 /etc/my.cnf
     sudo echo "bind-address=192.168.33.100" >>/etc/my.cnf
     sudo chmod 644 /etc/my.cnf
+    sudo systemctl restart mysqld
 }
 software_install
 database_config
